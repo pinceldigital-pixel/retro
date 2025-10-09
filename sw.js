693af1da0aaa94e9b5014e093da13338b442b77c
pinceldigital-pixel <pinceldigital@gmail.com>
2025-10-09 16:49:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'radio-pwa-v12';
+const CACHE = 'radio-pwa-v13';
 const CORE = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
